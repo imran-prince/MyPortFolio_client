3dd8e7925b324a6befc798684a76d98970656d04
imran-hossain <csebubt35@gmail.com>
2022-06-11 14:33:41 +0600

    update

--- src/Components/useGetProject.js
+++ src/Components/useGetProject.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 const useGetProject = () => {
     const [projects, setProject] = useState([])
     useEffect(() => {
-        fetch('http://localhost:5000/allproject')
+        fetch('https://aqueous-falls-30488.herokuapp.com/allproject')
             .then(rs => rs.json())
             .then(data => setProject(data))
     }, [])
